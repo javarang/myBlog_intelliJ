03677d6a2d0255374ef1e96541b6133929ee7cee
최현석 <evillord@outlook.com>
2020-12-29 15:10:28 +0900

    202012291510

--- myBlog/src/main/java/com/javarang/MyBlogApplication.java
+++ myBlog/src/main/java/com/javarang/MyBlogApplication.java
@@ -1,13 +1,34 @@
 package com.javarang;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyBlogApplication {
+public class MyBlogApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyBlogApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+		pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+		pbeEnc.setPassword("javarang"); //2번 설정의 암호화 키를 입력
+
+		String enc = pbeEnc.encrypt("scretKey"); //암호화 할 내용
+		//qqoRjSEcKeF4/JEQOyzD7Mx6Y3nvD118
+		/*
+			url: ENC(qqoRjSEcKeF4/JEQOyzD7Mx6Y3nvD118)
+			username: ENC(qqoRjSEcKeF4/JEQOyzD7Mx6Y3nvD118)
+			password: ENC(qqoRjSEcKeF4/JEQOyzD7Mx6Y3nvD118)
+		*/
+		System.out.println("enc = " + enc); //암호화 한 내용을 출력
+
+		//테스트용 복호화
+		String des = pbeEnc.decrypt(enc);
+		System.out.println("des = " + des);
+	}
 }
--- myBlog/src/main/java/com/javarang/config/WebMvcConfig.java
+++ myBlog/src/main/java/com/javarang/config/WebMvcConfig.java
@@ -1,7 +1,11 @@
 package com.javarang.config;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -18,5 +22,19 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addInterceptor(interceptor)
 				.addPathPatterns("/**")
 				.excludePathPatterns("/inter/**");
+	}
+	@Bean("jasyptStringEncryptor")
+	public StringEncryptor stringEncryptor() {
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword("javarang"); //암호화에 사용할 키 -> 중요
+		config.setAlgorithm("PBEWithMD5AndDES"); //사용할 알고리즘
+		config.setKeyObtentionIterations("1000");
+		config.setPoolSize("1");
+		config.setProviderName("SunJCE");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setStringOutputType("base64");
+		encryptor.setConfig(config);
+		return encryptor;
 	}
 }
